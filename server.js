198d9aaf252fef82a63ad0a0e64b2415d05589d6
andermendz <andersonmendz19@gmail.com>
2024-12-21 20:40:52 -0500

    railway test

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const cors = require('cors');
+
+// all origins
+server.use(cors());
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+}); 
